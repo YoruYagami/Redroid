5964baa65a51e85115f9eacce59f5f051824cc80
YoruYagami <70035442+YoruYagami@users.noreply.github.com>
2024-07-04 18:05:22 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # Redroid
-Pentest automation script via NOX Emulator
+
+Redroid is a comprehensive tool for managing and interacting with the Nox Android emulator. It provides various functionalities including installing tools, managing the Nox emulator, and running Frida scripts.
+
+> :warning: Redroid is in an early state of release. Many more options and tools will be added later on.
+
+## Features
+
+- Install essential tools like Frida, Objection, reFlutter, Jadx, apktool, and nuclei.
+- Manage the Nox emulator:
+  - Remove ads and bloatware.
+  - Install Burp Suite certificate.
+  - Install and run Frida Server.
+  - Open an ADB shell.
+  - Check if root is enabled.
+  - Manage proxy settings.
+- Run Frida scripts.
+
+## Requirements
+
+- Python 3.x
+- `adb` and Nox Player installed on your machine.
+
+## Installation
+
+1. Clone the repository:
+   ```sh
+   git clone https://github.com/yourusername/Redroid.git
+   cd Redroid
+   ```
+
+2. Install the required Python packages:
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+Run the `Redroid` script using Python:
+```sh
+python adb_nox_menu.py
+```
+
+### Main Menu
+
+- `1. Install Tools`
+  - `1. Frida`
+  - `2. Objection`
+  - `3. reFlutter`
+  - `4. Jadx`
+  - `5. Download apktool (.bat + .jar)`
+  - `6. Download nuclei`
+  - `7. Back`
+- `2. NOX Player Options`
+  - `1. Remove Ads From Nox emulator`
+  - `2. Install Burp Certificate`
+  - `3. Install Frida Server`
+  - `4. Run Frida Server`
+  - `5. Get ADB shell`
+  - `6. Check if root is enabled`
+  - `7. Print proxy status`
+  - `8. Set up/modify proxy`
+  - `9. Remove proxy`
+  - `10. Back`
+- `3. Frida`
+  - `1. List installed applications`
+  - `2. Back`
+- `4. Exit`
+
+### Detailed Options
+
+#### Install Tools
+
+- **Frida**: Installs Frida tools.
+- **Objection**: Installs Objection.
+- **reFlutter**: Installs reFlutter.
+- **Jadx**: Downloads the latest version of Jadx and places it in the `Redroid` folder on the desktop.
+- **Download apktool (.bat + .jar)**: Downloads the latest version of apktool and places it in the `Redroid` folder on the desktop. You need to move these files manually to the `C:\Windows` folder.
+- **Download nuclei**: Downloads the latest version of nuclei and places it in the `Redroid` folder on the desktop.
+
+#### NOX Player Options
+
+- **Remove Ads From Nox emulator**: Removes ads and bloatware from the Nox emulator.
+- **Install Burp Certificate**: Installs the Burp Suite certificate on the Nox emulator. You need to specify the port Burp Suite is using to intercept requests.
+- **Install Frida Server**: Installs the Frida server on the Nox emulator.
+- **Run Frida Server**: Runs the Frida server on the Nox emulator.
+- **Get ADB shell**: Opens an ADB shell.
+- **Check if root is enabled**: Checks if root access is enabled on the Nox emulator.
+- **Print proxy status**: Prints the current proxy status.
+- **Set up/modify proxy**: Sets up or modifies the proxy.
+- **Remove proxy**: Removes the proxy settings from the Nox emulator.
+
+#### Frida
+
+- **List installed applications**: Lists the installed applications on the Nox emulator.
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any bugs, improvements, or features you would like to add.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ redroid.py
@@ -0,0 +1,436 @@
+import os
+import subprocess
+import psutil
+import requests
+import socket
+import re
+import shutil
+import lzma
+from zipfile import ZipFile
+from OpenSSL import crypto
+from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup
+
+def find_nox_installation_path():
+    for process in psutil.process_iter(['pid', 'name', 'exe']):
+        if 'Nox.exe' in process.info['name']:
+            return os.path.dirname(process.info['exe'])
+    return None
+
+nox_installation_path = find_nox_installation_path()
+
+def connect_to_nox_adb(ip='127.0.0.1', port=62001):
+    if nox_installation_path:
+        adb_command = f'\"{nox_installation_path}\\nox_adb.exe\" connect {ip}:{port}'
+        result = subprocess.run(adb_command, shell=True, text=True, capture_output=True)
+        return result.stdout.strip()
+    else:
+        return "Nox player not installed."
+
+def open_adb_shell_from_nox():
+    if nox_installation_path:
+        adb_shell_command = f'\"{nox_installation_path}\\nox_adb.exe\" shell -t su'
+        print("Opening ADB Shell. Type 'exit' to return to the main menu.")
+        subprocess.run(adb_shell_command, shell=True)
+    else:
+        print("Nox player not installed.")
+
+def get_local_ipv4_addresses():
+    ip_list = []
+    for iface_name in psutil.net_if_addrs():
+        for iface in psutil.net_if_addrs()[iface_name]:
+            if iface.family == socket.AF_INET:
+                ip_list.append(iface.address)
+    return ip_list
+
+def try_download_certificate(ip, port):
+    cert_url = f"http://{ip}:{port}/cert"
+    input_der_file = "cacert.der"
+    output_pem_file = "9a5ba575.0"
+
+    try:
+        response = requests.get(cert_url)
+
+        if response.status_code == 200:
+            with open(input_der_file, "wb") as certificate_file:
+                certificate_file.write(response.content)
+            print(f"Burp Suite certificate downloaded successfully from {cert_url}.")
+
+            with open(input_der_file, "rb") as der_file:
+                der_data = der_file.read()
+                cert = crypto.load_certificate(crypto.FILETYPE_ASN1, der_data)
+
+            with open(output_pem_file, "wb") as pem_file:
+                pem_data = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+                pem_file.write(pem_data)
+
+            os.system(f'\"{nox_installation_path}\\nox_adb.exe\" root')
+            os.system(f'\"{nox_installation_path}\\nox_adb.exe\" remount')
+            os.system(f'\"{nox_installation_path}\\nox_adb.exe\" push {output_pem_file} /system/etc/security/cacerts/')
+            os.system(f'\"{nox_installation_path}\\nox_adb.exe\" shell chmod 644 /system/etc/security/cacerts/{output_pem_file}')
+            print("BurpSuite Certificate Installed Successfully in Nox Player")
+            return True
+        else:
+            print(f"Error: Unable to download the certificate from {cert_url}.")
+            return False
+
+    except ConnectionError:
+        print(f"Error: Burp Suite is not running or the proxy server is not on {ip}:{port}.")
+        return False
+    except Exception as e:
+        print(f"An unexpected error occurred: {str(e)}")
+        return False
+
+def install_burpsuite_certificate(port):
+    # Check localhost first
+    if try_download_certificate('127.0.0.1', port):
+        return
+
+    # Check other local IP addresses
+    ipv4_addresses = get_local_ipv4_addresses()
+    for ip in ipv4_addresses:
+        if ip != '127.0.0.1' and try_download_certificate(ip, port):
+            return
+
+    print("Failed to download the Burp Suite certificate from any local IP address.")
+
+def install_tool(tool):
+    subprocess.run(['pip', 'install', tool])
+
+def download_latest_jadx():
+    try:
+        response = requests.get("https://api.github.com/repos/skylot/jadx/releases/latest")
+        response.raise_for_status()
+        latest_release = response.json()
+        assets = latest_release.get('assets', [])
+        for asset in assets:
+            if 'no-jre-win.exe' in asset['name']:
+                download_url = asset['browser_download_url']
+                local_filename = asset['name']
+                
+                # Get the desktop path of the current user
+                desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop', 'Redroid')
+                os.makedirs(desktop_path, exist_ok=True)
+                local_filepath = os.path.join(desktop_path, local_filename)
+                
+                print(f"Downloading {local_filename} from {download_url}")
+                with requests.get(download_url, stream=True) as r:
+                    r.raise_for_status()
+                    with open(local_filepath, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=8192):
+                            f.write(chunk)
+                print(f"Downloaded {local_filename} to your Redroid folder on desktop: {local_filepath}")
+                return
+        print("No suitable Jadx executable found in the latest release.")
+    except Exception as e:
+        print(f"An error occurred while trying to download the latest version of Jadx: {str(e)}")
+
+def get_latest_apktool_url():
+    url = "https://bitbucket.org/iBotPeaches/apktool/downloads/"
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and href.endswith('.jar'):
+            return f"https://bitbucket.org{href}"
+    return None
+
+def setup_apktool():
+    try:
+        bat_url = "https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/windows/apktool.bat"
+        jar_url = get_latest_apktool_url()
+        if not jar_url:
+            print("Failed to find the latest apktool.jar")
+            return
+        
+        # Get the desktop path of the current user
+        desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop', 'Redroid')
+        os.makedirs(desktop_path, exist_ok=True)
+        
+        # Download apktool.bat
+        print(f"Downloading apktool.bat from {bat_url}")
+        response = requests.get(bat_url)
+        response.raise_for_status()
+        bat_path = os.path.join(desktop_path, "apktool.bat")
+        with open(bat_path, "wb") as file:
+            file.write(response.content)
+        
+        # Download apktool.jar
+        print(f"Downloading apktool.jar from {jar_url}")
+        response = requests.get(jar_url)
+        response.raise_for_status()
+        jar_path = os.path.join(desktop_path, "apktool.jar")
+        with open(jar_path, "wb") as file:
+            file.write(response.content)
+        
+        print(f"apktool setup completed. Files downloaded to your Redroid folder on desktop: {bat_path} and {jar_path}")
+        print("Please move apktool.bat and apktool.jar to the C:\\Windows folder manually.")
+    except Exception as e:
+        print(f"An error occurred while setting up apktool: {str(e)}")
+
+def download_latest_nuclei():
+    try:
+        latest_release_url = "https://api.github.com/repos/projectdiscovery/nuclei/releases/latest"
+        response = requests.get(latest_release_url)
+        response.raise_for_status()
+        latest_release = response.json()
+        for asset in latest_release['assets']:
+            if 'windows_amd64.zip' in asset['name']:
+                download_url = asset['browser_download_url']
+                local_filename = asset['name']
+                
+                # Get the desktop path of the current user
+                desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop', 'Redroid')
+                os.makedirs(desktop_path, exist_ok=True)
+                local_filepath = os.path.join(desktop_path, local_filename)
+                
+                print(f"Downloading {local_filename} from {download_url}")
+                with requests.get(download_url, stream=True) as r:
+                    r.raise_for_status()
+                    with open(local_filepath, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=8192):
+                            f.write(chunk)
+                
+                # Extract the zip file
+                with ZipFile(local_filepath, 'r') as zip_ref:
+                    zip_ref.extractall(desktop_path)
+                os.remove(local_filepath)
+                print(f"Downloaded and extracted Nuclei to your Redroid folder on desktop: {desktop_path}")
+                return
+        print("No suitable Nuclei executable found in the latest release.")
+    except Exception as e:
+        print(f"An error occurred while trying to download the latest version of Nuclei: {str(e)}")
+
+def check_nox_root():
+    if nox_installation_path:
+        adb_shell_command = f'\"{nox_installation_path}\\nox_adb.exe\" shell "su -c \'id\'"'
+        result = subprocess.run(adb_shell_command, shell=True, text=True, capture_output=True)
+        if 'uid=0(root)' in result.stdout:
+            print("Root is enabled on Nox Emulator.")
+        else:
+            print("Root is not enabled on Nox Emulator.")
+    else:
+        print("Nox player not installed.")
+
+def get_nox_proxy_status():
+    if nox_installation_path:
+        adb_shell_command = f'\"{nox_installation_path}\\nox_adb.exe\" shell settings get global http_proxy'
+        result = subprocess.run(adb_shell_command, shell=True, text=True, capture_output=True)
+        if result.stdout.strip():
+            print(f"Current proxy: {result.stdout.strip()}")
+        else:
+            print("No proxy is currently set.")
+    else:
+        print("Nox player not installed.")
+
+def set_nox_proxy(ip, port):
+    if nox_installation_path:
+        adb_shell_command = f'\"{nox_installation_path}\\nox_adb.exe\" shell settings put global http_proxy {ip}:{port}'
+        subprocess.run(adb_shell_command, shell=True, text=True)
+        print(f"Proxy set to {ip}:{port} on Nox Emulator.")
+    else:
+        print("Nox player not installed.")
+
+def remove_nox_proxy():
+    if nox_installation_path:
+        adb_shell_command = f'\"{nox_installation_path}\\nox_adb.exe\" shell settings delete global http_proxy'
+        subprocess.run(adb_shell_command, shell=True, text=True)
+        print("Proxy removed from Nox Emulator.")
+    else:
+        print("Nox player not installed.")
+
+def remove_ads_and_bloatware():
+    print("Removing Bloatware and Ads from Nox Emulator...")
+    debloatroot = f'\"{nox_installation_path}\\nox_adb.exe\" root'
+    os.system(debloatroot)
+    debloatremount = f'\"{nox_installation_path}\\nox_adb.exe\" remount'
+    os.system(debloatremount)    
+    bloatware_apps = [
+        'AmazeFileManager', 'AppStore', 'CtsShimPrebuilt', 'EasterEgg', 'Facebook',
+        'Helper', 'LiveWallpapersPicker', 'PrintRecommendationService', 'PrintSpooler',
+        'WallpaperBackup', 'newAppNameEn'
+    ]
+    for app in bloatware_apps:
+        os.system(f'\"{nox_installation_path}\\nox_adb.exe\" shell rm -rf /system/app/{app}')
+    print("Bloatware removed successfully.")
+
+    print("Rebooting the Nox Emulator...")
+    os.system(f'\"{nox_installation_path}\\nox_adb.exe\" shell su -c \'setprop ctl.restart zygote\'')
+    print("After successful reboot, configure your settings as needed.")
+
+def install_frida_server():
+    print("Checking Installed Frida-Tools Version")
+    frida_version_output = subprocess.check_output("frida --version 2>&1", shell=True, stderr=subprocess.STDOUT, text=True)
+    if re.search(r'(\d+\.\d+\.\d+)', frida_version_output):
+        frida_version = re.search(r'(\d+\.\d+\.\d+)', frida_version_output).group(1)
+        print(f"Frida-Tools Version: {frida_version}")
+        
+        noxarch = f'\"{nox_installation_path}\\nox_adb.exe\"  shell getprop ro.product.cpu.abi'
+        noxarchre = subprocess.run(noxarch, shell=True, text=True, check=True, capture_output=True)
+        noxarchresult = noxarchre.stdout.strip()
+        print(f"CPU Architecture of Nox Emulator: {noxarchresult}")
+        
+        print("Downloading Frida-Server With Same Version")
+        frida_server_url = f"https://github.com/frida/frida/releases/download/{frida_version}/frida-server-{frida_version}-android-{noxarchresult}.xz"
+        
+        response = requests.get(frida_server_url)
+        with open("frida-server.xz", "wb") as f:
+            f.write(response.content)
+        
+        with lzma.open("frida-server.xz") as f:
+            with open("frida-server", "wb") as out_f:
+                out_f.write(f.read())
+        
+        os.remove("frida-server.xz")
+        
+        os.system(f'\"{nox_installation_path}\\nox_adb.exe\" push frida-server /data/local/tmp/')
+        os.remove("frida-server")
+        
+        chmodfridaserver = f'\"{nox_installation_path}\\nox_adb.exe\"  shell chmod +x /data/local/tmp/frida-server'
+        os.system(chmodfridaserver)
+        print("Provided executable permissions to Frida Server.")
+        print("Frida Server setup completely on Nox Emulator.")
+        print()
+    else:
+        print("Frida Tools is not installed on this system.")
+
+def run_frida_server():
+    if nox_installation_path:
+        print("Frida Server is running...")
+        print("Below Some Useful commands of Frida-Tools")
+        print("List installed applications: frida-ps -Uai")
+        print("Frida Script Injection: frida -U -l fridascript.js -f com.package.name")
+        runfridaserver = f'\"{nox_installation_path}\\nox_adb.exe\"  shell /data/local/tmp/frida-server'
+        os.system(runfridaserver)        
+    else:
+        print("Frida server not started on the Nox Player.")
+
+def list_installed_applications():
+    print("Listing installed applications on Nox Emulator...")
+    os.system("frida-ps -Uai")
+
+def show_main_menu():
+    print("\nMain Menu")
+    print("1. Install Tools")
+    print("2. NOX Player Options")
+    print("3. Frida")
+    print("4. Exit")
+
+def show_install_tools_menu():
+    print("\nInstall Tools")
+    print("1. Frida")
+    print("2. Objection")
+    print("3. reFlutter")
+    print("4. Jadx")
+    print("5. Download apktool (.bat + .jar)")
+    print("6. Download nuclei")
+    print("7. Back")
+
+def show_nox_player_options_menu():
+    print("\nNOX Player Options")
+    print("1. Remove Ads From Nox emulator")
+    print("2. Install Burp Certificate")
+    print("3. Install Frida Server")
+    print("4. Run Frida Server")
+    print("5. Get ADB shell")
+    print("6. Check if root is enabled")
+    print("7. Print proxy status")
+    print("8. Set up/modify proxy")
+    print("9. Remove proxy")
+    print("10. Back")
+
+def show_frida_menu():
+    print("\nFrida")
+    print("1. List installed applications")
+    print("2. Back")
+
+def main():
+    while True:
+        show_main_menu()
+        main_choice = input("Enter your choice: ")
+
+        if main_choice == '1':
+            while True:
+                show_install_tools_menu()
+                tools_choice = input("Enter your choice: ")
+                
+                if tools_choice == '1':
+                    install_tool("frida-tools")
+                elif tools_choice == '2':
+                    install_tool("objection")
+                elif tools_choice == '3':
+                    install_tool("reFlutter")
+                elif tools_choice == '4':
+                    download_latest_jadx()
+                elif tools_choice == '5':
+                    setup_apktool()
+                elif tools_choice == '6':
+                    download_latest_nuclei()
+                elif tools_choice == '7':
+                    break
+                else:
+                    print("Invalid choice, please try again.")
+
+        elif main_choice == '2':
+            if nox_installation_path:
+                connection_result = connect_to_nox_adb()
+                print(connection_result)
+                if "connected" in connection_result.lower():
+                    while True:
+                        show_nox_player_options_menu()
+                        nox_choice = input("Enter your choice: ")
+
+                        if nox_choice == '1':
+                            remove_ads_and_bloatware()
+                        elif nox_choice == '2':
+                            port = input("Enter the port Burp Suite is using to intercept requests: ")
+                            if port.isdigit():
+                                install_burpsuite_certificate(int(port))
+                            else:
+                                print("Invalid port. Please enter a valid port number.")
+                        elif nox_choice == '3':
+                            install_frida_server()
+                        elif nox_choice == '4':
+                            run_frida_server()
+                        elif nox_choice == '5':
+                            open_adb_shell_from_nox()
+                        elif nox_choice == '6':
+                            check_nox_root()
+                        elif nox_choice == '7':
+                            get_nox_proxy_status()
+                        elif nox_choice == '8':
+                            ip = input("Enter the proxy IP address: ")
+                            port = input("Enter the proxy port: ")
+                            set_nox_proxy(ip, port)
+                        elif nox_choice == '9':
+                            remove_nox_proxy()
+                        elif nox_choice == '10':
+                            break
+                        else:
+                            print("Invalid choice, please try again.")
+                else:
+                    print("Unable to connect to Nox emulator. Please check if it is running and try again.")
+            else:
+                print("Nox player not installed or not running.")
+
+        elif main_choice == '3':
+            while True:
+                show_frida_menu()
+                frida_choice = input("Enter your choice: ")
+
+                if frida_choice == '1':
+                    list_installed_applications()
+                elif frida_choice == '2':
+                    break
+                else:
+                    print("Invalid choice, please try again.")
+
+        elif main_choice == '4':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+if __name__ == "__main__":
+    main()
